1b0509030b0d0f3b089037ee01339d69c517a9a9
blizzard7891 <bllizard7891@gmail.com>
2017-06-08 00:33:47 +0900

    parcel function proto

--- pages/account_manage.php
+++ pages/account_manage.php
@@ -37,7 +37,7 @@
 	</div>
 
 	<div class="col-lg-6">
-		<div class="panel panel-default">
+		<div class="panel panel-blue">
 			<div class="panel-heading">
 				<strong>수입 / 지출 입력</strong>
 			</div>
@@ -57,16 +57,23 @@
 								</div>
 							</div>
 							<div class="form-group">
-								<label>금액</label>
-								<input type="number" name="amount" class="form-control" required>
+								<label>현금/카드</label>
+								<div>
+									<label class="radio-inline">
+										<input type="radio" name="paymethod" value="cash" checked>현금
+									</label>
+									<label class="radio-inline">
+										<input type="radio" name="paymethod" value="card"> 카드
+									</label>
+								</div>
 							</div>
 							<div class="form-group">
-								<label>입력날짜</label>
-								<input type="date" name="writedate" class="form-control" required>
+								<label>금액</label>
+								<input type="number" name="amount" class="form-control" min="0" max="999999999999" required>
 							</div>
 							<div class="form-group">
 								<label>내역 설명</label>
-								<textarea class="form-control" name="description" rows="3" required></textarea>
+								<textarea class="form-control" name="description" rows="2" maxlength="50" required></textarea>
 							</div>
 							<div class="pull-right">
 								<button type="submit" class="btn btn-primary mr-3">내역입력</button>
@@ -80,19 +87,20 @@
 	</div>
 
 	<div class="col-lg-12">
-		<div class="panel panel-default">
+		<div class="panel panel-blue">
 			<div class="panel-heading">
-				<strong>자금내역</strong>
+				<strong>현금 자금내역</strong>
 			</div>
 			<div class="panel-body">
 				<table width="100%" class="table table-striped table-bordered table-hover mb-0" id="myTable">
 					<thead>
-						<th>순번</th>
-						<th>일자</th>
-						<th>설명</th>
-						<th>구분</th>
-						<th>금액</th>
-						<th>잔여액</th>
+						<th width="10%">순번</th>
+						<th width="10%">일자</th>
+						<th width="30%">설명</th>
+						<th width="10%">구분</th>
+						<th width="10%">현금/카드</th>
+						<th width="10%">금액</th>
+						<th width="10%">잔여액</th>
 					</thead>
 					<tbody>
 
@@ -103,21 +111,66 @@
 						{
 							while($row = oci_fetch_array($s,OCI_RETURN_NULLS + OCI_ASSOC))
 							{
+								$i =0;
 								echo "<tr>";
 								foreach ($row as $item) 
 								{
-									if($item == $row['FLAG'] && trim($item)==="0")
+									$i++;
+									
+									if($item == $row['FLAG'] && trim($item)==="00" && $i==4)
 										echo "<td>수입</td>.";
-									elseif($item == $row['FLAG'] && trim($item)==="1")
+									elseif($item == $row['FLAG'] && trim($item)==="01" && $i==4)
 										echo "<td>지출</td>";
+									elseif($item == $row['PAY_METHOD'] && trim($item)==="00" && $i==5)
+										echo "<td>현금</td>.";
+									elseif($item == $row['PAY_METHOD'] && trim($item)==="01" && $i==5)
+										echo "<td>카드</td>";
+									elseif($item == $row['REMAIN_AMOUNT'] && $item ==0)
+										echo "<td>0</td>";
 									else
 										echo "<td>".($item?htmlentities($item):'&nbsp;')."</td>";
 								}
 								echo "</tr>";
+								$i =0;
 							}
 						}
 
-						$query = "SELECT seq_num,change_date,description,flag,amount,remain_amount FROM MONEY_MANAGEMENT";
+						$query = "SELECT seq_num,TO_CHAR(change_date,'YYYY/MM/DD'),description,flag,pay_method,amount,remain_amount FROM MONEY_MANAGEMENT WHERE pay_method ='00'";
+						$s = oci_parse($conn,$query);
+						oci_execute($s);
+						do_fetch($s);
+
+						oci_close($conn);
+						?>
+
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+	
+	<div class="col-lg-12">
+		<div class="panel panel-blue">
+			<div class="panel-heading">
+				<strong>카드 자금내역</strong>
+			</div>
+			<div class="panel-body">
+				<table width="100%" class="table table-striped table-bordered table-hover mb-0" id="myTable1">
+					<thead>
+						<th width="10%">순번</th>
+						<th width="10%">일자</th>
+						<th width="30%">설명</th>
+						<th width="10%">구분</th>
+						<th width="10%">현금/카드</th>
+						<th width="10%">금액</th>
+						<th width="10%">잔여액</th>
+					</thead>
+					<tbody>
+
+						<?php
+						include_once("./db.php");
+
+						$query = "SELECT seq_num,TO_CHAR(change_date,'YYYY/MM/DD'),description,flag,pay_method,amount,remain_amount FROM MONEY_MANAGEMENT WHERE pay_method ='01'";
 						$s = oci_parse($conn,$query);
 						oci_execute($s);
 						do_fetch($s);
@@ -138,7 +191,7 @@
 	<!-- Metis Menu Plugin JavaScript -->
 	<script src="../js/metisMenu.js"></script>
 	<!-- DataTables JavaScript -->
-    <script src="../vendor/datatables/js/jquery.dataTables.min.js"></script>
+    <script src="../vendor/datatables/js/jquery.dataTables.js"></script>
     <script src="../vendor/datatables-plugins/dataTables.bootstrap.min.js"></script>
     <script src="../vendor/datatables-responsive/dataTables.responsive.js"></script>
 	<!-- Custom Theme JavaScript -->
@@ -147,6 +200,12 @@
 	<script>
 		$(document).ready(function() {
 			$('#myTable').DataTable({
+				responsive: true
+			});
+		});
+		
+		$(document).ready(function() {
+			$('#myTable1').DataTable({
 				responsive: true
 			});
 		});
--- pages/service_parcel_process.php
+++ pages/service_parcel_process.php
@@ -1,42 +1,40 @@
-<?php 
-include_once("./db.php");
+<?php
+include_once( "./db.php" );
 
-if(isset($_POST['invoice']))
-{	
-$invoice = $_POST['invoice'];
-$date = date("Y/m/d");
-$weight = $_POST['weight'];
-$price = 2500;
+if ( isset( $_POST[ 'invoice' ] ) ) {
+	$invoice = $_POST[ 'invoice' ];
+	$date = date( "Y/m/d" );
+	$weight = $_POST[ 'weight' ];
+	$price = 2500;
 
-$query = "INSERT INTO PARCEL (
-INVOICE_NUM,
-ACCEPT_DATE,
-WEIGHT,
-SHIPPING_PRICE 
-)
-VALUES (
-:invoice,
-TO_DATE(:wdate,'yyyy-mm-dd'),
-:weight,
-:price
-)";
+	$query = "INSERT INTO PARCEL (
+	INVOICE_NUM,
+	ACCEPT_DATE,
+	WEIGHT,
+	SHIPPING_PRICE 
+	)
+	VALUES (
+	:invoice,
+	TO_DATE(:wdate,'yyyy-mm-dd'),
+	:weight,
+	:price
+	)";
 
-$s = oci_parse($conn,$query);
+	$s = oci_parse( $conn, $query );
 
-oci_bind_by_name($s, ':invoice', $invoice);
-oci_bind_by_name($s, ':wdate', $date);
-oci_bind_by_name($s, ':weight', $weight);
-oci_bind_by_name($s, ':price', $price);
+	oci_bind_by_name( $s, ':invoice', $invoice );
+	oci_bind_by_name( $s, ':wdate', $date );
+	oci_bind_by_name( $s, ':weight', $weight );
+	oci_bind_by_name( $s, ':price', $price );
 
-oci_execute($s);
-oci_free_statement($s);
+	oci_execute( $s );
+	oci_free_statement( $s );
 
-oci_close($conn);
+	oci_close( $conn );
 
-echo("<script>location.replace('./service_parcel.php');</script>"); 
-}else
-{
+	echo( "<script>location.replace('./service_parcel.php');</script>" );
+} else {
 
 }
 
- ?>
+?>
