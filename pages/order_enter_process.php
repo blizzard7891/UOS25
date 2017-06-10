cfde4d8cd4342a5a1deb28fb92415d309ef33382
coconut <wnsspdl@naver.com>
2017-06-11 02:22:08 +0900

    주문상세팝업창, 반품구현

--- pages/order_enter_process.php
+++ pages/order_enter_process.php
@@ -1,84 +1,85 @@
 <?php
-	$prevPage = $_SERVER['HTTP_REFERER'];
-	include_once("./db.php");
-	$pname = $_POST['pname'];
-	$onum = $_POST['onum'];
-	$enterqty = $_POST['enterqty'];
-	$enterdate = date("Y/m/d");
-	$enterflag = "00";
+
+include_once("./db.php");
+$pname = $_POST['pname'];
+$onum = $_POST['onum'];
+$enterqty = $_POST['enterqty'];
+$enterdate = date("Y/m/d");
+$enterflag = "00";
 
 
-	$query1 = "SELECT count(*) FROM ENTER";
-	$s = oci_parse($conn,$query1);
-	oci_execute($s);
-	$res1 = oci_fetch_array($s,OCI_RETURN_NULLS + OCI_ASSOC);
-	$seq = $res1['COUNT(*)'];
-	oci_free_statement($s);
+$query1 = "SELECT count(*) FROM ENTER";
+$s = oci_parse($conn,$query1);
+oci_execute($s);
+$res1 = oci_fetch_array($s,OCI_RETURN_NULLS + OCI_ASSOC);
+$seq = $res1['COUNT(*)'];
+oci_free_statement($s);
 
-	$seq++;
+$seq++;
 
-	$query2 = "SELECT PROD_NUM FROM PRODUCT WHERE PROD_NAME = :pname";
-	$s = oci_parse($conn,$query2);
-	oci_bind_by_name($s, ':pname', $pname);
-	oci_execute($s);
-	$res2 = oci_fetch_array($s,OCI_RETURN_NULLS + OCI_ASSOC);
-	$enterproduct = $res2['PROD_NUM'];
-	$pnum = $enterproduct;
-	oci_free_statement($s);
+$query2 = "SELECT PROD_NUM FROM PRODUCT WHERE PROD_NAME = :pname";
+$s = oci_parse($conn,$query2);
+oci_bind_by_name($s, ':pname', $pname);
+oci_execute($s);
+$res2 = oci_fetch_array($s,OCI_RETURN_NULLS + OCI_ASSOC);
+$enterproduct = $res2['PROD_NUM'];
+$pnum = $enterproduct;
+oci_free_statement($s);
 
-	$query3 = "INSERT INTO ENTER (
-	SEQ_NUM,
-	ENT_DATE,
-	ENT_GROUP,
-	PROD_NUM,
-	QTY
-	)
-	VALUES (
-	:seq,
-	TO_DATE(:enterdate,'yyyy/mm/dd'),
-	:enterflag,
-	:enterproduct,
-	:enterqty
-	)";
+$query3 = "INSERT INTO ENTER (
+SEQ_NUM,
+ENT_DATE,
+ENT_GROUP,
+PROD_NUM,
+QTY
+)
+VALUES (
+:seq,
+TO_DATE(:enterdate,'yyyy/mm/dd'),
+:enterflag,
+:enterproduct,
+:enterqty
+)";
 
-	$s = oci_parse($conn,$query3);
+$s = oci_parse($conn,$query3);
 
-	oci_bind_by_name($s, ':seq', $seq);
-	oci_bind_by_name($s, ':enterdate', $enterdate);
-	oci_bind_by_name($s, ':enterflag', $enterflag);
-	oci_bind_by_name($s, ':enterproduct', $enterproduct);
-	oci_bind_by_name($s, ':enterqty', $enterqty);
-	
-	oci_execute($s);
-	oci_free_statement($s);
+oci_bind_by_name($s, ':seq', $seq);
+oci_bind_by_name($s, ':enterdate', $enterdate);
+oci_bind_by_name($s, ':enterflag', $enterflag);
+oci_bind_by_name($s, ':enterproduct', $enterproduct);
+oci_bind_by_name($s, ':enterqty', $enterqty);
 
-	$query4 = "UPDATE PRODUCT
-	SET STOCK_QTY = STOCK_QTY+:enterqty
-	WHERE PROD_NUM = :pnum
-	";
+oci_execute($s);
+oci_free_statement($s);
 
-	$s = oci_parse($conn, $query4);
+$query4 = "UPDATE PRODUCT
+SET STOCK_QTY = STOCK_QTY+:enterqty
+WHERE PROD_NUM = :pnum
+";
 
-	oci_bind_by_name($s, ':enterqty', $enterqty);
-	oci_bind_by_name($s, ':pnum', $pnum);
+$s = oci_parse($conn, $query4);
 
-	oci_execute($s);
-	oci_free_statement($s);
+oci_bind_by_name($s, ':enterqty', $enterqty);
+oci_bind_by_name($s, ':pnum', $pnum);
 
-	$query5 = "UPDATE ORDER_LIST
-	SET ENT_FLAG = 00
-	WHERE PROD_NUM = :pnum AND ORDER_NUM = :onum
-	";
+oci_execute($s);
+oci_free_statement($s);
 
-	$s = oci_parse($conn, $query5);
+$query5 = "UPDATE ORDER_LIST
+SET ENT_FLAG = 00
+WHERE PROD_NUM = :pnum AND ORDER_NUM = :onum
+";
 
-	oci_bind_by_name($s, ':pnum', $pnum);
-	oci_bind_by_name($s, ':onum', $onum);
+$s = oci_parse($conn, $query5);
 
-	oci_execute($s);
-	oci_free_statement($s);
+oci_bind_by_name($s, ':pnum', $pnum);
+oci_bind_by_name($s, ':onum', $onum);
 
-	oci_close($conn);
+oci_execute($s);
+oci_free_statement($s);
 
-    
+echo "<script type=\"text/javascript\">
+
+history.back();
+</script>";
 ?>
